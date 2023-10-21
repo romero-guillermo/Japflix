6906c3164aec9709a0748a02f951359cfa36ba5d
mmantaras <micaela.mantaras@gmail.com>
2023-10-21 16:34:33 -0300

    Se implementa el offcanvas con el dropdown para el detalle de la película

--- JAPFLIX/js/script.js
+++ JAPFLIX/js/script.js
@@ -9,32 +9,25 @@ fetch(apiUrl)
   })
   .then(data => {
     const lista = document.getElementById("lista");
-
-    // Almacenar los datos en una variable global para su uso posterior
     let peliculas = data;
 
-    // Función para mostrar una película en la lista
     const mostrarPeliculaEnLista = (pelicula) => {
       const elemento = document.createElement("li");
       elemento.classList.add("list-group-item");
 
-      // Título
       const titulo = document.createElement("h5");
       titulo.textContent = pelicula.title;
       elemento.appendChild(titulo);
 
-      // Tagline
       const tagline = document.createElement("p");
       tagline.textContent = pelicula.tagline;
       elemento.appendChild(tagline);
 
-      // Votación (estrellas)
       const votacion = document.createElement("span");
-      const estrellas = '\u2605'.repeat(Math.round(pelicula.vote_average / 2)); // Convierte el voto en estrellas
+      const estrellas = '\u2605'.repeat(Math.round(pelicula.vote_average / 2));
       votacion.textContent = estrellas;
       elemento.appendChild(votacion);
 
-      // Evento de clic
       elemento.addEventListener("click", () => {
         mostrarDetalle(pelicula);
       });
@@ -42,99 +35,65 @@ fetch(apiUrl)
       lista.appendChild(elemento);
     };
 
-    // Función para mostrar el detalle de una película
     const mostrarDetalle = (pelicula) => {
-      // Llenar el modal con los detalles de la película
-      const modalTitulo = document.getElementById("modalTitulo");
-      const modalGeneros = document.getElementById("modalGeneros");
-      const modalSinopsis = document.getElementById("modalSinopsis");
-      const modalAnio = document.getElementById("modalAnio");
-      const modalDuracion = document.getElementById("modalDuracion");
-      const modalPresupuesto = document.getElementById("modalPresupuesto");
-      const modalGanancias = document.getElementById("modalGanancias");
-      {
-        const detalleTitulo = document.getElementById("detalleTitulo");
-  const detalleSinopsis = document.getElementById("detalleSinopsis");
-  const detalleGeneros = document.getElementById("detalleGeneros");
-  const detalleBoton = document.getElementById("detalleBoton");
-  const detalleDesplegable = document.getElementById("detalleDesplegable");
+      const detalleTitulo = document.getElementById("detalleTitulo");
+      const detalleSinopsis = document.getElementById("detalleSinopsis");
+      const detalleGeneros = document.getElementById("detalleGeneros");
+      const detalleBoton = document.getElementById("detalleBoton");
+      const detalleDesplegable = document.getElementById("detalleDesplegable");
 
-  detalleTitulo.textContent = pelicula.title;
-  detalleSinopsis.textContent = pelicula.overview;
+      detalleTitulo.textContent = pelicula.title;
+      detalleSinopsis.textContent = pelicula.overview;
 
-  // Convertir el objeto de géneros a una cadena de texto
-  const generos = pelicula.genres.map((genero) => genero.name).join(", ");
-  detalleGeneros.textContent = "Géneros: " + generos;
+      const generos = pelicula.genres.map((genero) => genero.name).join(", ");
+      detalleGeneros.textContent = "Géneros: " + generos;
 
-  // Crear el botón con el desplegable
-  detalleBoton.innerHTML = `
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#detalleDesplegable" aria-expanded="false">
-      Detalles adicionales
-    </button>
-  `;
+      // Limpia el contenido actual del desplegable
+      detalleDesplegable.innerHTML = '';
 
-  // Llenar el desplegable con información adicional
-  detalleDesplegable.innerHTML = `
-    <div class="collapse" id="detalleDesplegable">
-      <p>Año de lanzamiento: ${pelicula.release_date.split("-")[0]}</p>
-      <p>Duración: ${pelicula.runtime} minutos</p>
-      <p>Presupuesto: $${pelicula.budget}</p>
-      <p>Ganancias: $${pelicula.revenue}</p>
-    </div>
-  `;
+      // Agrega nuevos elementos al desplegable
+      const detalles = [
+        `Año de lanzamiento: ${pelicula.release_date.split("-")[0]}`,
+        `Duración: ${pelicula.runtime} minutos`,
+        `Presupuesto: $${pelicula.budget}`,
+        `Ganancias: $${pelicula.revenue}`
+      ];
 
-  // Mostrar el contenedor de detalle
-  const peliculaDetalle = document.getElementById("peliculaDetalle");
-  peliculaDetalle.style.display = "block";
-};
+      detalles.forEach(detalle => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<a class="dropdown-item" href="#">${detalle}</a>`;
+        detalleDesplegable.appendChild(listItem);
+      });
 
-
-      // Mostrar el contenedor de detalle
-      const peliculaDetalle = document.getElementById("peliculaDetalle");
-      peliculaDetalle.style.display = "block";
-
-      modalTitulo.textContent = pelicula.title;
-      modalGeneros.textContent = pelicula.genres.join(", ");
-      modalSinopsis.textContent = pelicula.overview;
-      modalAnio.textContent = pelicula.release_date.split("-")[0];
-      modalDuracion.textContent = pelicula.runtime + " minutos";
-      modalPresupuesto.textContent = `$${pelicula.budget}`;
-      modalGanancias.textContent = `$${pelicula.revenue}`;
-
-      // Mostrar el modal
-      $("#modalDetalle").modal("show");
+      // Muestra el offcanvas
+      const offcanvasDetalle = new bootstrap.Offcanvas(document.getElementById('offcanvasDetalle'));
+      offcanvasDetalle.show();
     };
 
     peliculas.forEach((pelicula) => {
-        const elemento = document.createElement("li");
-        elemento.classList.add("list-group-item");
+      const elemento = document.createElement("li");
+      elemento.classList.add("list-group-item");
 
-        // Título
-        const titulo = document.createElement("h5");
-        titulo.textContent = pelicula.title;
-        elemento.appendChild(titulo);
+      const titulo = document.createElement("h5");
+      titulo.textContent = pelicula.title;
+      elemento.appendChild(titulo);
 
-        // Tagline
-        const tagline = document.createElement("p");
-        tagline.textContent = pelicula.tagline;
-        elemento.appendChild(tagline);
+      const tagline = document.createElement("p");
+      tagline.textContent = pelicula.tagline;
+      elemento.appendChild(tagline);
 
-        // Votación (estrellas)
-        const votacion = document.createElement("span");
-        const estrellas = '\u2605'.repeat(Math.round(pelicula.vote_average / 2)); // Convierte el voto en estrellas
-        votacion.textContent = estrellas;
-        elemento.appendChild(votacion);
+      const votacion = document.createElement("span");
+      const estrellas = '\u2605'.repeat(Math.round(pelicula.vote_average / 2));
+      votacion.textContent = estrellas;
+      elemento.appendChild(votacion);
 
-        // Evento de clic para mostrar detalle
-        elemento.addEventListener("click", () => {
-          mostrarDetalle(pelicula);
-        });
-
-        lista.appendChild(elemento);
+      elemento.addEventListener("click", () => {
+        mostrarDetalle(pelicula);
       });
 
+      lista.appendChild(elemento);
+    });
 
-    // Evento de búsqueda
     const inputBuscar = document.getElementById("inputBuscar");
     const btnBuscar = document.getElementById("btnBuscar");
 
@@ -143,7 +102,6 @@ fetch(apiUrl)
       lista.innerHTML = "";
 
       if (valor) {
-        // Filtrar películas que coincidan con la búsqueda
         const peliculasFiltradas = peliculas.filter(pelicula =>
           pelicula.title.toLowerCase().includes(valor) ||
           pelicula.genres.join(", ").toLowerCase().includes(valor) ||
@@ -153,14 +111,12 @@ fetch(apiUrl)
 
         peliculasFiltradas.forEach(mostrarPeliculaEnLista);
       } else {
-        // Mostrar todas las películas si no hay búsqueda
         peliculas.forEach(mostrarPeliculaEnLista);
       }
-      // Mostrar la lista de películas después de una búsqueda
+
       lista.style.display = "block";
     });
 
-    // Ocultar la lista de películas en la carga inicial
     lista.style.display = "none";
   })
   .catch(error => {
